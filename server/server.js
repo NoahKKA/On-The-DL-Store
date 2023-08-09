a20109bc859ae0af8e7dff4f6b81c0110e160c46
NoahKKA <noahhhhh.a@gmail.com>
2023-08-08 17:52:47 -0700

    changed server.js because config.js was in that file

--- server/server.js
+++ server/server.js
@@ -1,27 +1,21 @@
-require('dotenv').config()
+//Dependencies
+const express =  require('express')
+const app = express()
+const { Sequelize } = require('sequelize')
+const path = require('path')
+const cors = require('cors')
 
-module.exports = {
-  development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    login: true,
-    dialect: "postgres"
-  },
-  test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: "postgres"
-  },
-  production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    login: true,
-    dialect: "postgres"
-  }
-}
+//Conifguration/ Middleware
+require('dotenv').config()
+app.use(cors())
+app.use(express.json)
+app.use(express.urlencoded({ extended:false }))
+app.use(express.static(path.join(__dirname, '../build')))
+
+app.listen(4005, () => {
+  console.log("Server running on port 4005")
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'))
+})
