950ce9cb7e749870368755cb95c800b05ea54bff
NoahKKA <noahhhhh.a@gmail.com>
2023-08-08 19:18:39 -0700

    fixed bug where server wasnt starting

--- server/server.js
+++ server/server.js
@@ -1,21 +1,23 @@
-//Dependencies
-const express =  require('express')
+//DEPENDENCIES
+const express = require('express')
 const app = express()
 const { Sequelize } = require('sequelize')
 const path = require('path')
 const cors = require('cors')
 
-//Conifguration/ Middleware
+//MIDDLEWARE
 require('dotenv').config()
 app.use(cors())
-app.use(express.json)
-app.use(express.urlencoded({ extended:false }))
+app.use(express.json())
+app.use(express.urlencoded({ extended:false }));
 app.use(express.static(path.join(__dirname, '../build')))
 
+// LISTEN
 app.listen(4005, () => {
-  console.log("Server running on port 4005")
+    console.log("Server is running on port 4005")
 })
 
+// 404 Page
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'))
-})
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
